ae630536ac0f317b796200fc64310a2630ddbdc9
namiqmamedov <namigmm@code.edu.az>
2023-06-02 22:27:28 +0400

    Updated about page

--- src/components/UI/about-slider/AboutSlider.jsx
+++ src/components/UI/about-slider/AboutSlider.jsx
@@ -12,6 +12,27 @@ import icon9 from '../../../assets/about/icon9.svg'
 import icon10 from '../../../assets/about/icon10.svg'
 import icon11 from '../../../assets/about/icon11.svg'
 import icon12 from '../../../assets/about/icon12.svg'
+import icon13 from '../../../assets/about/icon13.svg'
+import icon14 from '../../../assets/about/icon14.svg'
+import icon15 from '../../../assets/about/icon15.svg'
+import icon16 from '../../../assets/about/icon16.svg'
+import icon17 from '../../../assets/about/icon17.svg'
+import icon18 from '../../../assets/about/icon18.svg'
+import arrow01 from '../../../assets/about/arrow1.svg'
+import arrow02 from '../../../assets/about/arrow2.svg'
+import arrow03 from '../../../assets/about/arrow3.svg'
+import arrow04 from '../../../assets/about/arrow4.svg'
+import arrow05 from '../../../assets/about/arrow5.svg'
+import arrow06 from '../../../assets/about/arrow6.svg'
+import arrow07 from '../../../assets/about/arrow7.svg'
+import arrow08 from '../../../assets/about/arrow8.svg'
+import arrow09 from '../../../assets/about/arrow9.svg'
+import arrow10 from '../../../assets/about/arrow10.svg'
+import arrow11 from '../../../assets/about/arrow11.svg'
+import arrow12 from '../../../assets/about/arrow12.svg'
+import arrow13 from '../../../assets/about/arrow13.svg'
+import arrow14 from '../../../assets/about/arrow14.svg'
+import arrow15 from '../../../assets/about/arrow15.svg'
 import Slider from "react-slick";
 import {GrLinkNext} from 'react-icons/gr'
 import {GrLinkPrevious} from 'react-icons/gr'
@@ -21,7 +42,7 @@ import "slick-carousel/slick/slick-theme.css";
 const AboutSlider = () => {
     const settings = {
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         draggable: true,
         prevArrow: <button><GrLinkPrevious className='prev-btn'/></button>,
@@ -31,7 +52,7 @@ const AboutSlider = () => {
             breakpoint: 1200,
             settings: {
               arrows: true,
-              slidesToShow: 3,
+              slidesToShow: 1,
             },
           },
           {
@@ -56,12 +77,13 @@ const AboutSlider = () => {
               },
             },
         ],
-      };  
+    };  
   return (
    <div className="about__slider">
      <div className='container'>
         <div className="row">
-            <div className="col-12">
+           <Slider {...settings}>
+           <div className="about__slider-order">
                 <h3 className='about-head'>Klassik üsul</h3>
                 <p className='about-text'>Əsl şampan, Champenoise(şampeno)  texnologiyasından
                   istifadə etməklə hazırlanır. İkinci dərəcəli şüşə fermentasiyasının
@@ -77,16 +99,19 @@ const AboutSlider = () => {
                             <img src={icon1} alt="Icon" />
                             <p>Üzümün əl ilə yığımı</p>
                         </div>
+                        <img className='arrow' src={arrow01} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon2} alt="Icon" />
                             <p>
                              Küve əldə etmək üçün üzümün zərif sıxılması (İlkin sıxılan şirə)
                              </p>
                         </div>
+                        <img className='arrow' src={arrow02} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon3} alt="Icon" />
                             <p>İlkin fermentasiya (əsas şərabın hazırlanması)</p>
                         </div>
+                        <img className='arrow' src={arrow03} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon4} alt="Icon" />
                             <p>Yığılma (istənilən əsas növə nail olmaq üçün müxtəlif sortların şərablarını qarışdırmaq)</p>
@@ -95,20 +120,21 @@ const AboutSlider = () => {
                      <div className="about__item-order">
                         <div className="item-wrapper">
                             <img src={icon5} alt="Icon" />
-                            <p>
-                                             Maya və şəkərin əlavə edilməsi. Şüşələrə doldurulması
-                                        </p>
+                            <p>Maya və şəkərin əlavə edilməsi. Şüşələrə doldurulması</p>
                         </div>
+                        <img className='arrow' src={arrow04} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon6} alt="Icon" />
                             <p>
                             Şüşə içərisində ikincili fermentasiya
                              </p>
                         </div>
+                        <img className='arrow' src={arrow05} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon7} alt="Icon" />
                             <p>İlkin fermentasiya (əsas şərabın hazırlanması)</p>
                         </div>
+                        <img className='arrow' src={arrow06} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon8} alt="Icon" />
                             <p>Remuaj (şüşələri tədricən alt-üst etmək).</p>
@@ -119,23 +145,85 @@ const AboutSlider = () => {
                             <img src={icon9} alt="Icon" />
                             <p>Çöküntünü duzlu suya sallaraq şüşənin dondurulması</p>
                         </div>
+                        <img className='arrow' src={arrow07} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon10} alt="Icon" />
                             <p>
                             Deqorjaj (çöküntünün çıxarılması)
                              </p>
                         </div>
+                        <img className='arrow' src={arrow08} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon11} alt="Icon" />
                             <p>Doza (doza likyoru əlavə etməklə həcmin artırılması) 
                              və təbii prob ilə örtülməsi</p>
                         </div>
+                        <img className='arrow' src={arrow09} alt="arrow icon" />
                         <div className="item-wrapper">
                             <img src={icon12} alt="Icon" />
                             <p>Son olaraq bir neçə ay saxlamaq</p>
                         </div>
                      </div>
             </div>
+            <div className="about__slider-order">
+                <h3 className='about-head'>Çarmat üsul</h3>
+                <p className='about-text'>Bu, çənlərdə oynaq şərab istehsal etmək üsuludur. Onun fərqi ondan ibarətdir ki, 
+                ikincili fermentasiya böyük germetik qablarda- rezervuarlar və ya akratoforlar da 21 gün ərzində baş verir. 
+                Çarmat üsulu ilə «Русское игристое» kolleksiyası yaradılır.</p>
+                     <div className="about__item-order">
+                        <div className="item-wrapper">
+                            <img src={icon1} alt="Icon" />
+                            <p>Üzümün əl ilə yığımı</p>
+                        </div>
+                        <img className='arrow' src={arrow01} alt="arrow icon" />
+                        <div className="item-wrapper">
+                            <img src={icon2} alt="Icon" />
+                            <p>
+                             Küve əldə etmək üçün üzümün zərif sıxılması (İlkin sıxılan şirə)
+                             </p>
+                        </div>
+                        <img className='arrow' src={arrow02} alt="arrow icon" />
+                        <div className="item-wrapper">
+                            <img src={icon3} alt="Icon" />
+                            <p>İlkin fermentasiya (əsas şərabın hazırlanması)</p>
+                        </div>
+                     </div>
+                     <div className="about__item-order">
+                     <div className="item-wrapper">
+                            <img src={icon13} alt="Icon" />
+                            <p>Müxtəlif üzüm sortlarından şərabların yığılması, şəkər və maya əlavə edilməsi</p>
+                      </div>
+                        <img className='arrow' src={arrow10} alt="arrow icon" />
+                        <div className="item-wrapper">
+                            <img src={icon14} alt="Icon" />
+                            <p>Maya və şəkərin əlavə edilməsi. Şüşələrə doldurulması</p>
+                        </div>
+                        <img className='arrow' src={arrow11} alt="arrow icon" />
+                        <div className="item-wrapper">
+                            <img src={icon15} alt="Icon" />
+                            <p>
+                            Şüşə içərisində ikincili fermentasiya
+                             </p>
+                        </div>
+                     </div>
+                     <div className="about__item-order">
+                     <div className="item-wrapper">
+                            <img src={icon16} alt="Icon" />
+                            <p>Remuaj (şüşələri tədricən alt-üst etmək).</p>
+                        </div>
+                        <img className='arrow' src={arrow12} alt="arrow icon" />
+                     <div className="item-wrapper">
+                            <img src={icon17} alt="Icon" />
+                            <p>İlkin fermentasiya (əsas şərabın hazırlanması)</p>
+                        </div>
+                            <img className='arrow' src={arrow13} alt="arrow icon" />
+                        <div className="item-wrapper">
+                            <img src={icon18} alt="Icon" />
+                            <p>Çöküntünü duzlu suya sallaraq şüşənin dondurulması</p>
+                        </div>
+                     </div>
+            </div>
+           </Slider>
         </div>
     </div>
    </div>
